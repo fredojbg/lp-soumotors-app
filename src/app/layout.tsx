749552f07c38b227f1a81fc74d6c825f5eb2b48d
Alfredo Jose <alfredojose1022@gmail.com>
2024-08-02 11:35:57 -0300

    refactor: Update page title and description for Sou Motors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,11 +6,48 @@ import '@/styles/tailwind.css'
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - SouMotors',
-    default: 'SouMotors - Controle Especializado no Ramo Automotivo',
+    template: '%s - Sou Motors',
+    default: 'Sou Motors - Controle Especializado no Ramo Automotivo',
   },
+
   description:
-    'Descubra soluções inovadoras para crescimento e gestão no setor de estética automotiva com a SouMotorsApp.',
+    'Descubra soluções inovadoras para crescimento e gestão no setor de estética automotiva com a Sou Motors.',
+  keywords: [
+    'Sou Motors',
+    'Sistema de Gestão Automotiva',
+    'Sistema para oficinas mecânicas',
+    'Estética Automotiva',
+    'Controle de Estoque',
+    'Controle Financeiro',
+    'Controle de Vendas',
+    'Controle de Serviços',
+    'Controle de Clientes',
+    'Controle de Funcionários',
+    'Controle de Fornecedores',
+    'Controle de Produtos',
+    'Controle de Compras',
+    'Controle de Orçamentos',
+    'Controle de Agendamentos',
+    'Controle de Relatórios',
+    'Controle de Comissões',
+    'Controle de Despesas',
+  ],
+  openGraph: {
+    title: 'Sou Motors - Sistema de Gestão Automotiva',
+    description:
+      'Descubra soluções inovadoras para crescimento e gestão no setor de estética automotiva com a Sou Motors.',
+    url: 'https://soumotors.com',
+    siteName: 'Sou Motors',
+    images: [
+      {
+        url: 'https://www.soumotors.com/_next/image?url=/_next/static/media/dashboard.5b61f4c3.png&w=2048&q=75',
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 }
 
 const inter = Inter({
