81d98cbec1d9d32d777169069b8a4a058e5ea1a8
Alfredo Jose <alfredojose1022@gmail.com>
2024-09-27 00:05:16 -0300

    refactor: Update chat-crisp and Facebook pixel scripts in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,6 +16,18 @@ export default function Home() {
           'window.$crisp=[];window.CRISP_WEBSITE_ID="173766c7-3164-4941-8ff1-a6edeaa4e39a";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();'
         }
       </Script>
+      <Script>
+        {`!function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1446512262649898');
+          fbq('track', 'PageView');`}
+      </Script>
       <Header />
       <main>
         <Hero />
